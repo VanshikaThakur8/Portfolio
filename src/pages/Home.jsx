fc74493be07fcd773e0e0b1c642f64f26249ae1b
VanshikaThakur8 <vanshikathakur930@gmail.com>
2025-06-18 23:25:08 +0530

    pdf changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import profile from '../assets/profile.jpg'; 
 import { useNavigate } from 'react-router-dom';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import resumePDF from '../assets/resume.pdf';
+import resumePDF from '../assets/Resume.pdf';
 
 
 
